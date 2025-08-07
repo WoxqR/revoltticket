c8d56c179df14b243127380dc38215058870e2f7
WoxqR <yagizmertesmer@hotmail.com>
2025-08-07 17:19:36 +0300

    config.js

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-
+module.exports = {
+  token: "",
+  channel: "1402584489095331871",      // destek mesajının atılacağı kanal
+  staff: "1240286625150402661"     // destekçi rolü ya da kullanıcı idsi
+};
