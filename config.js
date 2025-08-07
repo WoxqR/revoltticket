df6054fb8736c432880642083a190df815258f5f
WoxqR <yagizmertesmer@hotmail.com>
2025-08-07 17:36:14 +0300

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "",
-  channel: "1402584489095331871",      // destek mesajının atılacağı kanal
-  staff: "1240286625150402661"     // destekçi rolü ya da kullanıcı idsi
+  token: process.env.TOKEN,
+  channel: process.env.CHANNEL,
+  staff: process.env.STAFF
 };
